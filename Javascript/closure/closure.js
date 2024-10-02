045a81207f0332938bf252953443bc076fd3f805
rondido <rppr001@naver.com>
2024-10-02 23:54:51 +0900

    closue 개념 되짚어보기 정리중

---
+++ Javascript/closure/closure.js
@@ -0,0 +1,55 @@
+// 함수를 인수로 전달받고 함수를 반환하는 고차 함수
+// 이 함수는 카운트 상태를 유지하기 위한 자유 변수 counter를 기억하는 클로저를 반환
+function makeCounter(aux) {
+  // 카운트 상태를 유지하기 위한 자유 변수
+  let counter = 0;
+  //클로저를 반환
+  return function () {
+    //인수로 전달받은 보조 함수에 상태 변경을 위임.
+    counter = aux(counter);
+    return counter;
+  };
+}
+// 보조 함수
+function increase(n) {
+  return ++n;
+}
+
+function decrease(n) {
+  return --n;
+}
+
+// 함수로 함수를 생성
+//makeCounter 함수는 보조 함수를 인수로 전달받아 함수를 반환
+const increaser = makeCounter(increase); // 1번
+console.log(increaser()); //1
+console.log(increaser()); //2
+
+//increaser 함수와는 별개의 독립된 렉시컬 환경을 갖기 때문에 카운터 상태가 연동 되지 않는다.
+const decreaser = makeCounter(decrease); // 2번
+console.log(decreaser()); // -1
+console.log(decreaser()); // -2
+
+// function outer() {
+//   let x = 10;
+
+//   inner();
+// }
+
+// function inner() {
+//   console.log(x);
+// }
+// outer();
+
+const x = 1;
+function foo() {
+  const x = 10;
+  bar();
+}
+
+function bar() {
+  console.log(x);
+}
+
+foo(); //?
+bar(); //?
--- Javascript/this/this.js
+++ Javascript/this/this.js
@@ -161,6 +161,8 @@ function getThisBinding() {
   return this;
 }
 
+
+
 //getThisBinding 함수를 호출하면서 인수로 전달한 객체를 getThisBinding 함수의 this에 바인딩한다.
 console.log(getThisBinding.bind(thisArg)); //{a:1}
 console.log(getThisBinding.call(thisArg)()); //{a:1}
